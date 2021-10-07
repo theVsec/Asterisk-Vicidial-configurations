4cf1f08967992e57dc17c2ddf56052b3e23d21a3
theVsec <92098987+theVsec@users.noreply.github.com>
2021-10-07 15:39:47 +0200

    Update get_user.php

--- call_landing/get_user.php
+++ call_landing/get_user.php
@@ -14,7 +14,7 @@ $mysql_db='database';
 // Find the last user who called this number
 $link = $mysql = mysql_connect($serveri, $mysql_user, $mysql_pass, $mysql_db) or die(mysql_error());
 mysql_select_db($mysql_db, $mysql);
-$result = mysql_query("select last_local_call_time,user from vicidial_list where phone_number = $phone and last_local_call_time not NULL order by last_local_call_time desc limit 0,1;");
+$result = mysql_query("select last_local_call_time,user from vicidial_list where phone_number = $phone and last_local_call_time is not NULL order by last_local_call_time desc limit 0,1;");
 
 $row1 = mysql_fetch_array ( $result );
 echo $row1['user'];
