96ade6659a6713e6728266663921ec07699296f7
theVsec <92098987+theVsec@users.noreply.github.com>
2021-10-07 15:15:25 +0200

    Create get_user.php

---
+++ get_user.php
@@ -0,0 +1,21 @@
+<?php
+// Get the phone variable
+
+$phone=$_GET['phone'];
+
+// Connection data
+
+$serveri='x.x.x.x';
+$mysql_user='user';
+$mysql_pass='password';
+$mysql_db='database';
+
+
+// Gjejme operatorin qe do te marre telefonaten
+$link = $mysql = mysql_connect($serveri, $mysql_user, $mysql_pass, $mysql_db) or die(mysql_error());
+mysql_select_db($mysql_db, $mysql);
+$result = mysql_query("select last_local_call_time,user from vicidial_list where phone_number = $phone and last_local_call_time not NULL order by last_local_call_time desc limit 0,1;");
+
+$row1 = mysql_fetch_array ( $result );
+echo $row1['user'];
+?>
