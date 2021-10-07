2a2abc49f3f62c44fc20fa25559ebc108a864e4c
theVsec <92098987+theVsec@users.noreply.github.com>
2021-10-07 15:16:50 +0200

    Update get_user.php

--- get_user.php
+++ get_user.php
@@ -11,7 +11,7 @@ $mysql_pass='password';
 $mysql_db='database';
 
 
-// Gjejme operatorin qe do te marre telefonaten
+// Find the last user who called this number
 $link = $mysql = mysql_connect($serveri, $mysql_user, $mysql_pass, $mysql_db) or die(mysql_error());
 mysql_select_db($mysql_db, $mysql);
 $result = mysql_query("select last_local_call_time,user from vicidial_list where phone_number = $phone and last_local_call_time not NULL order by last_local_call_time desc limit 0,1;");
